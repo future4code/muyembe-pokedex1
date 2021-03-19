d5546d194078026570ad2e897580372213b20742
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-03-18 22:52:48 -0300

    pastas criadas

---
+++ pokedex-projeto10/src/components/detalhe/telaDetalhes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Button from '@material-ui/core/Button'
+
+
+const TelaDetalhe = () => {
+    return(
+        <div>
+            <Button variant="contained" color="primary">Clique aqui!</Button>
+        </div>
+    )
+}
+
+export default TelaDetalhe;
