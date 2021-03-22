4154f8f81fb54c19894ecbadc9014a41b4ae7d97
luizfbarbosa12 <l.nandoferbarbosa@gmail.com>
2021-03-21 22:16:23 -0300

    progress bar

--- pokedex-projeto10/src/components/detalhe/containerPrincipal/containerPrincipal.js
+++ pokedex-projeto10/src/components/detalhe/containerPrincipal/containerPrincipal.js
@@ -3,23 +3,37 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-import {useState} from 'react'
+import { useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../../requisições/requisicoes";
+import { Progress } from "../progressBar/progressBar";
+import {StyledP} from  './styles'
 
 export default function SimpleContainer() {
-  const [pokemon, setPokemon] = useState([])
-  const [pokemonImage, setPokemonImage] = useState('')
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonImage, setPokemonImage] = useState("");
 
-  useEffect(()=> {
-    axios.get(`${BASE_URL}/pokemon/bulbasaur`).then((response) => {
-      console.log(response)
-      setPokemon(response)
-      setPokemonImage(response.data.sprites.versions['generation-v']['black-white'].animated.front_default)
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
+
+  useEffect(() => {
+    buscaPokemon()
+}, []);
+
+  const buscaPokemon = () => {
+    axios
+      .get(`${BASE_URL}/pokemon/bulbasaur`)
+      .then((response) => {
+        console.log(response);
+        setPokemon(response);
+        setPokemonImage(
+          response.data.sprites.versions["generation-v"]["black-white"].animated
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+ 
 
   return (
     <React.Fragment>
@@ -33,6 +47,7 @@ export default function SimpleContainer() {
           justifyContent: "center",
         }}
       >
+        
         <div>
           <Box
             style={{
@@ -44,11 +59,18 @@ export default function SimpleContainer() {
               backgroundColor: "#232323",
               color: "#f2f2f2",
               margin: "20px",
+              boxShadow: '0 0 60px rgba(0, 0, 0, 0.9)',
+              display: 'flex',
+              placeContent: 'center',
+              placeItems: 'center'
             }}
           >
-            <img style={{width: '15vw', height: '30vh'}}
-              src={pokemonImage}
+            <img
+              style={{ width: "9vw", height: "20vh" }}
+              src={pokemonImage.front_default}
+
             />
+            
           </Box>
           <Box
             style={{
@@ -60,10 +82,15 @@ export default function SimpleContainer() {
               backgroundColor: "#232323",
               color: "#f2f2f2",
               margin: "20px",
+              boxShadow: '0 0 60px rgba(0, 0, 0, 0.9)',
+              display: 'flex',
+              placeContent: 'center',
+              placeItems: 'center'
             }}
           >
-            <img style={{width: '15vw', height: '30vh'}}
-              src={"https://cdn.bulbagarden.net/upload/e/e9/Spr_b_5b_001.png"}
+            <img
+              style={{ width: "8vw", height: "20vh" }}
+              src={pokemonImage.back_default}
             />
           </Box>
         </div>
@@ -77,16 +104,28 @@ export default function SimpleContainer() {
             backgroundColor: "#232323",
             color: "#f2f2f2",
             margin: "20px",
+            boxShadow: '0 0 60px rgba(0, 0, 0, 0.9)'
           }}
         >
           <p>Stats</p>
-          <p>HP</p>
-          <p>Attack</p>
-          <p>defense</p>
-          <p>special-attack</p>
-          <p>special-defense</p>
-          <p>speed</p>
-
+          
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              placeContent: "start-end",
+              placeItems: "flex-start",
+              marginLeft: '1vw',
+            }}
+          >
+            
+            <StyledP>HP:{<Progress done={pokemon.data.stats[0].base_stat}/>}</StyledP>
+            <StyledP>Attack: {<Progress done={pokemon.data.stats[1].base_stat}/>}</StyledP>
+            <StyledP>Defense:{<Progress done={pokemon.data.stats[2].base_stat}/>}</StyledP>
+            <StyledP>SP Attack: {<Progress done={pokemon.data.stats[3].base_stat}/>}</StyledP>
+            <StyledP>SP Defense:{<Progress done={pokemon.data.stats[4].base_stat}/>}</StyledP>
+            <StyledP>Speed:{<Progress done={pokemon.data.stats[5].base_stat}/>}</StyledP>
+          </div>
         </Box>
         <div>
           <Box
@@ -99,9 +138,11 @@ export default function SimpleContainer() {
               backgroundColor: "#232323",
               color: "#f2f2f2",
               margin: "20px",
+              boxShadow: '0 0 60px rgba(0, 0, 0, 0.9)'
             }}
           >
-            <p>Type: Grass</p>
+            {/* <p>{`Type: ${pokemon.data.types[0].type.name}`} and {pokemon.data.types[1].type.name} 
+            </p> */}
           </Box>
           <Box
             style={{
@@ -113,12 +154,21 @@ export default function SimpleContainer() {
               backgroundColor: "#232323",
               color: "#f2f2f2",
               margin: "20px",
+              boxShadow: '0 0 60px rgba(0, 0, 0, 0.9)'
             }}
-          >
+          > <div style={{
+            display: "flex",
+            flexDirection: "column",
+            placeContent: "start-end",
+            placeItems: "flex-start",
+            marginLeft: '2vw',
+            marginTop: '4vh'
+          }}> 
             <p>Moves</p>
             <p>Vine whip</p>
             <p>Razor-leaf</p>
             <p>Solar Beam</p>
+            </div>
           </Box>
         </div>
         <Typography
---
+++ pokedex-projeto10/src/components/detalhe/containerPrincipal/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const StyledP = styled.p`
+    display: flex;
+    flex-direction: column;
+    margin: 1.5vh;
+`
